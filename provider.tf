f35c493ba835162c49a172ea48398c1b5663270f
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-12 16:56:52 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "karo-terraform-state"
+    bucket = "kolly-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "eu-west-2"
     dynamo_table = "terraform-state"

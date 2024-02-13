3bf119a88b9ed8f62fa7d2457fa49f1f2c4d19a2
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-13 16:47:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -149,10 +149,9 @@ resource "aws_route_table_association" "private_subnet_route_table_association_2
   route_table_id = aws_route_table.private_subnet_route_table.id
 }
 
-
 # Create an IAM role for the EKS cluster
-resource "aws_iam_role" "mp1-eks_cluster_role" {
-  name = "mp1-eks_cluster_role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -170,16 +169,16 @@ resource "aws_iam_role" "mp1-eks_cluster_role" {
 # Attach the necessary policies to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.mp1-eks_cluster_role.name
+  role       = aws_iam_role.eks_cluster_role.name
 }
 
 # Create an EKS cluster
- resource "aws_eks_cluster" "mopops1_cluster" {
-  name = "mopops1_cluster"
-  role_arn = aws_iam_role.mp1-eks_cluster_role.arn
+resource "aws_eks_cluster" "karo_cluster" {
+  name     = "eks_cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
   version  = "1.26"
 
-   vpc_config {
+  vpc_config {
     subnet_ids = [aws_subnet.eks_priv_sub_one.id,
       aws_subnet.eks_priv_sub_two.id,
       aws_subnet.eks_pub_sub_one.id,
@@ -191,15 +190,17 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   ]
 }
 
- #Export the kubeconfig for the EKS cluster
- #output "kubeconfig" {
- #value = aws_eks_cluster.mopops1_cluster.kubeconfig
+# Export the kubeconfig for the EKS cluster
+# output "kubeconfig" {
+#  value = aws_eks_cluster.karo_cluster.kubeconfig
 #}
 
 
+
+
 # Create an IAM role for the worker nodes
-resource "aws_iam_role" "mp1-eks_worker_node_role" {
-  name = "mp1-eks_worker_node_role"
+resource "aws_iam_role" "eks_worker_node_role" {
+  name = "eks_worker_node_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -217,26 +218,26 @@ resource "aws_iam_role" "mp1-eks_worker_node_role" {
 # Attach the necessary policies to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.mp1-eks_worker_node_role.name
+  role       = aws_iam_role.eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.mp1-eks_worker_node_role.name
+  role       = aws_iam_role.eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_ec2CR_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.mp1-eks_worker_node_role.name
+  role       = aws_iam_role.eks_worker_node_role.name
 }
 
 
 
 # Create the EKS node group
 resource "aws_eks_node_group" "eks_node" {
-  cluster_name    = aws_eks_cluster.mopops1_cluster.name
+  cluster_name    = aws_eks_cluster.karo_cluster.name
   node_group_name = "eks_node"
-  node_role_arn   = aws_iam_role.mp1-eks_worker_node_role.arn
+  node_role_arn   = aws_iam_role.eks_worker_node_role.arn
 
   # Subnet configuration
   subnet_ids = [

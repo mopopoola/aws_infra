6f2c07a2861a624c7539633527601462a556c4d1
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-13 09:35:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -169,13 +169,13 @@ resource "aws_iam_role" "mp-eks_cluster_role" {
 # Attach the necessary policies to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.mp-eks_cluster_role.name
 }
 
 # Create an EKS cluster
 resource "aws_eks_cluster" "karo_cluster" {
   name     = "eks_cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = aws_iam_role.mp-eks_cluster_role.arn
   version  = "1.26"
 
   vpc_config {
@@ -218,17 +218,17 @@ resource "aws_iam_role" "mp-eks_worker_node_role" {
 # Attach the necessary policies to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_worker_node_role.name
+  role       = aws_iam_role.mp-eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_worker_node_role.name
+  role       = aws_iam_role.mp-eks_worker_node_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_ec2CR_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_worker_node_role.name
+  role       = aws_iam_role.mp-eks_worker_node_role.name
 }
 
 
@@ -237,7 +237,7 @@ resource "aws_iam_role_policy_attachment" "eks_ec2CR_policy_attachment" {
 resource "aws_eks_node_group" "eks_node" {
   cluster_name    = aws_eks_cluster.karo_cluster.name
   node_group_name = "eks_node"
-  node_role_arn   = aws_iam_role.eks_worker_node_role.arn
+  node_role_arn   = aws_iam_role.mp-eks_worker_node_role.arn
 
   # Subnet configuration
   subnet_ids = [

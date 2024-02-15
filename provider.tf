d63984a2159cfe8f2b8aeb23ffa0de084339c840
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-15 15:29:27 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mopops1-terraform-state"
+    bucket = "hannah1-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "eu-west-2"
     dynamodb_table = "terraform-state"

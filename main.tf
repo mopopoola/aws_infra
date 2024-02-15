554a3aefce9e5e8008314b9e7e82e0c5e9da57e9
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-15 16:09:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -173,7 +173,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
 }
 
 # Create an EKS cluster
-resource "aws_eks_cluster" "eks_cluster" {
+resource "aws_eks_cluster" "karo_cluster" {
   name     = "eks_cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
   version  = "1.26"
@@ -235,7 +235,7 @@ resource "aws_iam_role_policy_attachment" "eks_ec2CR_policy_attachment" {
 
 # Create the EKS node group
 resource "aws_eks_node_group" "eks_node" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
+  cluster_name    = aws_eks_cluster.karo_cluster.name
   node_group_name = "eks_node"
   node_role_arn   = aws_iam_role.eks_worker_node_role.arn
 

b5714de296c39d816f1248794c880f3b0377f6ae
mopopoola <78879877+mopopoola@users.noreply.github.com>
2024-02-16 15:06:53 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  backend "s3" {
+    bucket = "hannah1-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-state"
+    encrypt = true
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
